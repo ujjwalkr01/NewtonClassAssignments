6bc42696a2c168b76106fb77f72d5f990af48af1
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-01-17 11:35:13 +0530

    commit

---
+++ NewtonSchool_Assignments/src/One_DArray_assign/ArrayisSorted_rotated.java
@@ -0,0 +1,49 @@
+package One_DArray_assign;
+
+import java.util.Scanner;
+
+public class ArrayisSorted_rotated {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		checkarray(arr, n);
+		if (checkarray(arr, n)) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+	}
+
+	private static boolean checkarray(int[] arr, int n) {
+
+		if (arr[n - 1] < arr[0]) {
+			int count = 0;
+			for (int i = 1; i < n; i++) {
+				if (arr[i] < arr[i - 1]) {
+					count++;
+				}
+			}
+			if (count == 1)
+				return true;
+		} else if (arr[n - 1] > arr[0]) {
+			int count = 0;
+			for (int i = 1; i < n; i++) {
+				if (arr[i] > arr[i - 1]) {
+					count++;
+				}
+			}
+			if (count == 1)
+				return true;
+		}
+		return false;
+	}
+
+}
